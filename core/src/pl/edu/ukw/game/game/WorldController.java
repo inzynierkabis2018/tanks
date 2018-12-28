de262cb296bf560d13f83fe78e18af2ab318bfa8
Tomasz Truszkowski <tomasz.truszkowski2013@gmail.com>
2018-12-28 16:55:07 +0100

    updated world controller class to use networking

--- core/src/pl/edu/ukw/game/game/WorldController.java
+++ core/src/pl/edu/ukw/game/game/WorldController.java
@@ -2,14 +2,24 @@ package pl.edu.ukw.game.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.edu.ukw.game.TanksClient;
 import pl.edu.ukw.game.game.objects.PlayerTank;
+import pl.edu.ukw.networkingentities.PlayerInfo;
+import pl.edu.ukw.networkingentities.Response;
 
 public class WorldController {
 
     private static final String TAG = WorldController.class.getName();
 
-    public PlayerTank playerOne;
+    public PlayerTank player = new PlayerTank();
+    public PlayerTank ally = new PlayerTank();
+    private List<Long> gameRooms = new ArrayList<Long>();
 
     public WorldController() {
         init();
@@ -20,7 +30,47 @@ public class WorldController {
     }
 
     private void init() {
-        playerOne = new PlayerTank(PlayerTank.PlayerNumber.ONE);
+
+        TanksClient.instance.addListener(new Listener() {
+
+            @Override
+            public void received(Connection connection, Object object) {
+                if (object instanceof Response.GameRoomsList) {
+                    Response.GameRoomsList gameRoomsList = (Response.GameRoomsList) object;
+                    gameRooms = gameRoomsList.getGameRooms();
+                }
+
+                if (object instanceof Response.JoinToGameRoom) {
+                    Response.JoinToGameRoom response = (Response.JoinToGameRoom) object;
+                    PlayerInfo playerInfo = response.getPlayerInfo();
+                    player.setPlayerInfo(playerInfo);
+                    player.connect();
+                    TanksClient.instance.allyJoined(playerInfo);
+                }
+
+                if (object instanceof Response.AllyJoinedToRoom) {
+                    Response.AllyJoinedToRoom response = (Response.AllyJoinedToRoom) object;
+                    PlayerInfo allyInfo = response.getPlayerInfo();
+                    ally.setPlayerInfo(allyInfo);
+                    ally.connect();
+                    TanksClient.instance.sendPosition(player);
+                }
+
+                if (object instanceof PlayerInfo) {
+                    PlayerInfo playerInfo = (PlayerInfo) object;
+                    ally.setPlayerInfo(playerInfo);
+                    ally.connect();
+                }
+
+                if (object instanceof Response.AllyLeftRoom) {
+                    ally.disconnect();
+                }
+            }
+        });
+
+        TanksClient.instance.login();
+        TanksClient.instance.createGameRoom();
+        TanksClient.instance.joinToGameRoom(1L);
     }
 
     private void handleInputGame(float deltaTime) {
@@ -29,25 +79,21 @@ public class WorldController {
 
     private void handlePlayerInput(float deltaTime) {
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            playerOne.setDirection(PlayerTank.TankDirection.LEFT);
-            playerOne.position.x -= 1 * deltaTime;
-            showPlayerPosition(playerOne);
+            player.setDirection(PlayerInfo.TankDirection.LEFT);
+            player.position.x -= 1 * deltaTime;
+            TanksClient.instance.sendPosition(player);
         } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            playerOne.setDirection(PlayerTank.TankDirection.RIGHT);
-            playerOne.position.x += 1 * deltaTime;
-            showPlayerPosition(playerOne);
+            player.setDirection(PlayerInfo.TankDirection.RIGHT);
+            player.position.x += 1 * deltaTime;
+            TanksClient.instance.sendPosition(player);
         } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            playerOne.setDirection(PlayerTank.TankDirection.DOWN);
-            playerOne.position.y -= 1 * deltaTime;
-            showPlayerPosition(playerOne);
+            player.setDirection(PlayerInfo.TankDirection.DOWN);
+            player.position.y -= 1 * deltaTime;
+            TanksClient.instance.sendPosition(player);
         } else if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            playerOne.setDirection(PlayerTank.TankDirection.UP);
-            playerOne.position.y += 1 * deltaTime;
-            showPlayerPosition(playerOne);
+            player.setDirection(PlayerInfo.TankDirection.UP);
+            player.position.y += 1 * deltaTime;
+            TanksClient.instance.sendPosition(player);
         }
-    }
-
-    private void showPlayerPosition(PlayerTank player) {
-        Gdx.app.debug(TAG, player.toString());
     }
 }
--- core/src/pl/edu/ukw/game/game/objects/PlayerTank.java
+++ core/src/pl/edu/ukw/game/game/objects/PlayerTank.java
@@ -4,31 +4,32 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import pl.edu.ukw.game.game.Assets;
+import pl.edu.ukw.networkingentities.PlayerInfo;
 
 public class PlayerTank extends AbstractGameObject {
 
     private TextureRegion tankX;
     private TextureRegion tankY;
-    private TankDirection direction;
-    private PlayerNumber playerNumber;
+    private PlayerInfo.TankDirection tankDirection;
+    private PlayerInfo.PlayerNumber playerNumber;
+    private boolean connected;
 
-    public PlayerTank(PlayerNumber playerNumber) {
-        this.playerNumber = playerNumber;
-        init();
+    public PlayerTank() {
+
     }
 
     @Override
     public void render(SpriteBatch batch) {
         TextureRegion region;
 
-        if (direction.equals(TankDirection.RIGHT) || direction.equals(TankDirection.LEFT)) {
+        if (tankDirection.equals(PlayerInfo.TankDirection.RIGHT) || tankDirection.equals(PlayerInfo.TankDirection.LEFT)) {
             region = tankX;
         } else {
             region = tankY;
         }
 
-        boolean flipY = direction.equals(TankDirection.DOWN);
-        boolean flipX = direction.equals(TankDirection.RIGHT);
+        boolean flipY = tankDirection.equals(PlayerInfo.TankDirection.DOWN);
+        boolean flipX = tankDirection.equals(PlayerInfo.TankDirection.RIGHT);
         batch.draw(
                 region.getTexture(), position.x, position.y, origin.x, origin.y,
                 dimension.x, dimension.y, scale.x, scale.y, rotation,
@@ -44,18 +45,50 @@ public class PlayerTank extends AbstractGameObject {
                 '}';
     }
 
-    public void setDirection(TankDirection direction) {
-        this.direction = direction;
+    public void setDirection(PlayerInfo.TankDirection direction) {
+        this.tankDirection = direction;
     }
 
-    public void setPlayerNumber(PlayerNumber playerNumber) {
+    public PlayerInfo.TankDirection getDirection() {
+        return tankDirection;
+    }
+
+    public void setPlayerNumber(PlayerInfo.PlayerNumber playerNumber) {
         this.playerNumber = playerNumber;
+    }
+
+    public PlayerInfo.PlayerNumber getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void connect() {
+        if (!isConnected()) {
+            connected = true;
+        }
+    }
+
+    public void disconnect() {
+        if (isConnected()) {
+            connected = false;
+        }
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setPlayerInfo(PlayerInfo playerInfo) {
+        position.x = playerInfo.getX();
+        position.y = playerInfo.getY();
+        playerNumber = playerInfo.getPlayerNumber();
+        tankDirection = playerInfo.getTankDirection();
+        init();
     }
 
     private void init() {
         dimension.set(0.5f, 0.5f);
 
-        if (playerNumber.equals(PlayerNumber.ONE)) {
+        if (playerNumber.equals(PlayerInfo.PlayerNumber.ONE)) {
             tankX = Assets.instance.playerOneTank.tankX;
             tankY = Assets.instance.playerOneTank.tankY;
         } else {
@@ -63,15 +96,6 @@ public class PlayerTank extends AbstractGameObject {
             tankY = Assets.instance.playerTwoTank.tankY;
         }
 
-        direction = TankDirection.UP;
         bounds.set(0, 0, dimension.x, dimension.y);
-    }
-
-    public enum PlayerNumber {
-        ONE, TWO
-    }
-
-    public enum TankDirection {
-        UP, DOWN, LEFT, RIGHT
     }
 }
