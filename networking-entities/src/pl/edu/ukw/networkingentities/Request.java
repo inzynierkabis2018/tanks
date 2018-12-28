38b0419f3847f6da93243701d55d20b2418f1367
Tomasz Truszkowski <tomasz.truszkowski2013@gmail.com>
2018-12-28 16:51:01 +0100

    request objects to exchange between server and client

--- networking-entities/src/pl/edu/ukw/networkingentities/Request.java
+++ networking-entities/src/pl/edu/ukw/networkingentities/Request.java
@@ -1,4 +1,46 @@
 package pl.edu.ukw.networkingentities;
 
 public class Request {
+
+    public static class Login {
+
+    }
+
+    public static class CreateGameRoom {
+
+    }
+
+    public static class JoinToGameRoom {
+        private Long gameRoomID;
+
+
+        public JoinToGameRoom() {
+
+        }
+
+        public JoinToGameRoom(Long gameRoomID) {
+           this.gameRoomID = gameRoomID;
+        }
+
+        public Long getGameRoomID() {
+            return gameRoomID;
+        }
+    }
+
+    public static class AllyJoinedToRoom {
+
+        private PlayerInfo playerInfo;
+
+        public AllyJoinedToRoom() {
+
+        }
+
+        public AllyJoinedToRoom(PlayerInfo playerInfo) {
+            this.playerInfo = playerInfo;
+        }
+
+        public PlayerInfo getPlayerInfo() {
+            return playerInfo;
+        }
+    }
 }
